dd9554ec96fe61bd49cc7c11fc32d58b37a0438c
Mironshoh <mironshoh1545@gmail.com>
2022-07-21 22:20:48 +0500

    .\arithmetic_op02.py

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -5,3 +5,11 @@
 # Create a variable called 'answer'.
 
 # Find the sum of a and b than assign to 'answer'
+
+from multiprocessing.connection import answer_challenge
+
+
+a = 3
+b = 4
+answer = a + b
+print (answer)
