40a2209958c9edf306a7f4a360fc7fe2088c9d6b
Mironshoh <mironshoh1545@gmail.com>
2022-07-21 21:59:53 +0500

    .\arithmetic_op01.py

--- arithmetic_op01.py
+++ arithmetic_op01.py
@@ -4,7 +4,7 @@
 
 # Find the sum of  'num_one' and 'num_two' then assign to 'variable_total'
 
-num_one = 5 
+num_one  =  5 
 num_two = 4
 variable_total = num_one + num_two
-print(variable_total)
+print(variable_total) 
